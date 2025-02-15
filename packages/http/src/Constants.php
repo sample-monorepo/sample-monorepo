8d1953f9353e6c531379bdb82b7de35d8b4a269e
Philip Gichuhi <pgichuhi@microsoft.com>
2025-02-15 11:04:08 +0300

    bump version

--- packages/http/src/Constants.php
+++ packages/http/src/Constants.php
@@ -5,5 +5,5 @@ namespace Microsoft\Kiota\Http;
 final class Constants
 {
     /** @var string The current version for this Library */
-    public const VERSION = '1.4.2';
+    public const VERSION = '1.4.3';
 }
